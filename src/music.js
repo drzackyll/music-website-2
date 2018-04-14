fb85455ef146a9544bd8baf3738c785d21a5c807
drzackyll <adams.z.d@gmail.com>
2018-04-13 22:15:42 -0400

    Updates for release

--- src/music.js
+++ src/music.js
@@ -10,8 +10,8 @@ const Music = () => {
           </div>
           <div className="col-md-5">
             <h2>Down the Line</h2>
-            A new album from Zack Adams now available for preorder on Vinyl, Compact Disc, and high bitrate Digital Download at
-            Zack's Limited Run store.<br /><br />
+            A new album from Zack Adams now available on Vinyl, Compact Disc, and high bitrate Digital Download at
+            Zack's Limited Run store, as well as on most streaming platforms.<br /><br />
             Tracklist:<br />
             <h6>1. American Joke</h6>
             <h6>2. Got My Story Straight</h6>
@@ -22,9 +22,7 @@ const Music = () => {
             <h6>7. Beachcomber</h6>
             <h6>8. So Much to Lose</h6>
             <h6>9. Can't Hide My Delight</h6>
-            <a href="http://zackadams.limitedrun.com/" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-outline-success">Preorder Now</button></a><br /><br />
-            {/* <a href="https://itunes.apple.com/us/album/down-the-line/1353729227" rel="noopener noreferrer" target="_blank"><img style={{width: "22%"}} src={require("./images/itunes-preorder.png")}  alt="Preorder on iTunes" /></a> */}
-            {/* <a href="https://www.amazon.com/Down-Line-Zack-Adams/dp/B07B4SM245/ref=sr_1_2?s=dmusic&ie=UTF8&qid=1520306794&sr=1-2-mp3-albums-bar-strip-0&keywords=Zack+Adams" rel="noopener noreferrer" target="_blank"><img style={{width: "22%", height: "7%"}} src={require("./images/amazon-preorder.png")}  alt="Preorder on Amazon" /></a><br /> */}
+            <a href="http://fanlink.to/DtheL" target="_blank" rel="noopener noreferrer"><button type="button" className="btn btn-outline-success">Listen Now</button></a><br /><br />
           </div>
         </div>
       </div>
